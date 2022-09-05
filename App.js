e43dc408aaffcee26ac8ed4a770a96119983a18e
Amiodas <amio.das.dip@gmail.com>
2022-09-05 23:18:59 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
+import Header from "./Components/Header";
+import AddTodo from "./Components/AddTodo";
+import TodoItem from "./Components/TodoItem";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "Buy Coffee", key: "1" },
+    { text: "Create an app", key: "2" },
+    { text: "Play on the switch", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      console.log(prevTodos);
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+  const addTodoHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        console.log(todos);
+        return [{ text: text, key: Math.random().toString }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("Opps!", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Header */}
+      <Header />
+      <View style={styles.content}>
+        {/* to Form */}
+        <AddTodo addTodoHandler={addTodoHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +52,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 20,
+  },
+  list: {
+    marginTop: 20,
   },
 });
